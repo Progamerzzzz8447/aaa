9a6ca69742f8839dbd9d3deaec6241041823a5bd
Progamerzzzz8447 <JH2010DH2013@HOTMAIL.COM>
2024-10-14 16:12:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["HolidayInn|Roblox","HolidayInn|Roblox"];
+const statusMessages = ["HolidayInn | Roblox","HolidayInn | Roblox"];
 
 
 let currentIndex = 0;
