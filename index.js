6000e7bc5590f89e57011f384096672130f37072
Progamerzzzz8447 <JH2010DH2013@HOTMAIL.COM>
2024-10-14 16:03:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["KFC | Roblox","KFC | Roblox"];
+const statusMessages = ["HolidayInn|Roblox","HolidayInn|Roblox"];
 
 
 let currentIndex = 0;
